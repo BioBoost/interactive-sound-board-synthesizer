24bff5454a27f9b4ee3aa8dbfeb24fe949348ac7
Tristan De Lil <tristan.delil@student.vives.be>
2022-10-20 17:26:26 +0200

    added-octave4-with-lookup-dictionary

--- sound.py
+++ sound.py
@@ -1,17 +1,17 @@
 #import the MQTT class
 from mqttclient import MQTT
 from synthesize import Synthesize
+import time
 
 synth = Synthesize()
 
 mqtt = MQTT("mqtt.devbit.be",1883, 60)
 mqtt.subcribe("test/soundboard/esp1", "test/soundboard/esp2", "test/soundboard/esp3", "test/soundboard/esp4")
 mqtt.start()
-      
+
 while(True):
 	print('Buzzy...')
-	synth.setVolume(mqtt.getTopic1())
-	synth.setDuration(mqtt.getTopic2())
+	synth.selectChords(mqtt.getTopic1())
 	print(synth.getVolume())
 	print(synth.getDuration())
 	synth.playChords()
--- synthesize.py
+++ synthesize.py
@@ -1,12 +1,14 @@
 from synthesizer import Player, Synthesizer, Waveform
-#Make a class to save the instance 
+import numpy as np
 
 class Synthesize:
 	def __init__(self):
 		self.__volume = 0.5
 		self.__duration = 0.5
 		self.__pitch = 1 #afblijven van de pitch hier moet code nog voor getest worden
-		self.__chord = 0
+		self.__currentchord = 0
+		self.__chord = [1,1]
+
 		self.__player = Player()
 		self.__sin = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
 		self.__player.open_stream()
@@ -33,13 +35,32 @@ class Synthesize:
 	def getPitch(self):
 		return self.__pitch
 
+	def selectChords(self,note1):
+		octave4 = {1:(256.8912) , 2:(272.13184) , 3:(288.3152) , 4:(305.5984) , 5:(323.6672) , 6:(342.99296) , 7:(363.26144) , 8:(384.944) , 9:(407.88352) , 10:(432.08) , 11:(457.84768) , 12:(485.02944)}
+		note1 = round(float(note1) * 12)
+		if note1 < 1:
+			note1 = 1
+		self.__chord[0] = octave4[int(note1)]
+
+		#print(self.__chord)
+		return self
+
 	def playChords(self):
+		#Octave4 = [256.8912 , 272.13184 , 288.3152 , 305.5984 , 323.6672 , 342.99296 , 363.26144 , 384.944 , 407.88352 , 432.08 , 457.84768 , 485.02944]
+		for note in self.__chord:
+			self.__sin._osc1._volume = float(self.getVolume())
+			self.__player.play_wave(self.__sin.generate_constant_wave(note, self.getDuration()))
+			self.__currentchord += 1
+
+
+	def playChordstest(self):
 		
-		Octave4 = [261 , 277.18 , 293.66 , 311.13 , 329.63 , 349.23 , 369.99 , 392 , 415.3 , 440 , 466.16 , 493.88]
-		if self.__chord >= len(Octave4):
-			self.__chord = 0
+		Octave4 = [256.8912 , 272.13184 , 288.3152 , 305.5984 , 323.6672 , 342.99296 , 363.26144 , 384.944 , 407.88352 , 432.08 , 457.84768 , 485.02944]
+
+		if self.__currentchord >= len(Octave4):
+			self.__currentchord = 0
 		
 		#niet aan raken tot mqtt in orde is
 		self.__sin._osc1._volume = float(self.getVolume())
-		self.__player.play_wave(self.__sin.generate_constant_wave(Octave4[self.__chord], self.getDuration()))
-		self.__chord += 1
+		self.__player.play_wave(self.__sin.generate_constant_wave(Octave4[self.__currentchord], self.getDuration()))
+		self.__currentchord += 1
