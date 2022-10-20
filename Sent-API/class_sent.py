2355b7985780c4a89119172233b56599790a5fa2
r0755466 <r0755466@gmail.com>
2022-10-20 15:25:48 +0200

    Added topics, read

--- Sent-API/class_sent.py
+++ Sent-API/class_sent.py
@@ -1,56 +1,62 @@
 # defines a node in a singly linked list
 
-import numpy as np
-import random as random
 import paho.mqtt.client as mqtt
 
-# defines a singly linked list
 class MQTT:
     #De values we define from the main 
-    def __init__(self, broker = None, port = None, stopseconds = None):
-        self.__broker = broker # current Node 
-        self.__port = port
-        self.__stopseconds = stopseconds
-        self.client = mqtt.Client()
-        self.client.connect(broker, port, stopseconds)
+    def __init__(self, broker , port , stopseconds):
+        self.__topic1 = 1
+        self.__topic2 = 1
+        self.__topic3 = 1
+        self.__topic4 = 1
+        self.__client = mqtt.Client()
+        self.__client.connect(broker, port, stopseconds)
         
-    
-    def subcribe(self, topic1, topic2, topic3 ):
-        self.client.subscribe(topic1)
-        self.client.subscribe(topic2)
-        self.client.subscribe(topic3)
+    # We can expand to multple topics from the main
+    def subcribe(self, topics):
+        for topic in topics: 
+            self.__client.subscribe(topic)
+           
+    def on_connect(self , client, userdata, flags, rc):
+         print("Connected with result code "+ str(rc))
 
-    def on_connect(client, userdata, flags, rc):
-         print("Connected with result code "+str(rc))
-
-    def on_message(client, userdata, msg):
+    def on_message(self ,client, userdata, msg):
         if(msg.topic == 'test/soundboard/esp1'):
-            volume = msg.payload.decode()
+            self.topic1 = msg.payload.decode()
             print(f"Message received [{msg.topic}]: {msg.payload}")
         
         if(msg.topic == 'test/soundboard/esp2'):
-            pitch = msg.payload.decode()
+            self.topic2 = msg.payload.decode()
             print(f"Message received [{msg.topic}]: {msg.payload}")
 
-    def get_client(self): 
-        return self.client
+        if(msg.topic == 'test/soundboard/esp3'):
+            self.topic3 = msg.payload.decode()
+            print(f"Message received [{msg.topic}]: {msg.payload}")
 
-    def get_userdata(self): 
-        return self.userdata
+        if(msg.topic == 'test/soundboard/esp4'):
+            self.__topic4 = msg.payload.decode()
+            print(f"Message received [{msg.topic}]: {msg.payload}")
 
-    def get_msg(self): 
-        return self.msg
-    
-    def get_flags(self): 
-        return self.flags
 
-    def set_client(self, client): 
-        self.client = client
-        #self.client.on_connect = on_connect
-        #self.client.on_message = on_message
+    # We use get to call the value of every topic, soo we van do what we want. 
+    # We normalize from esp the value
+    def getTopic1(self):
+        return self.__topic1
 
-    def client_start(self):
-        self.client.loop_start()
+    def getTopic2(self):
+        return self.__topic2
+
+    def getTopic3(self):
+        return self.__topic3
+
+    def getTopic4(self):
+        return self.__topic4
+
+    def start(self):
+        self.__client.loop_start()
+        # we call from client object , the on_connect method and we overwrited
+        self.__client.on_connect = self.on_connect
+        self.__client.on_message = self.on_message
 
     
         
--- Sent-API/main.py
+++ Sent-API/main.py
@@ -1,20 +1,9 @@
 import time
-from synthesizer import Player, Synthesizer, Waveform
+#from synthesizer import Player, Synthesizer, Waveform
 import numpy as np
 import random as random
-import paho.mqtt.client as mqtt
 #For address calling
 import ctypes
-
-
-#Voor later expand 
-def run(): 
-      mqtt = MQTT("mqtt.devbit.be",1883, 60)
-      mqtt.subcribe("test/soundboard/esp1", "test/soundboard/esp2", "test/soundboard/esp3")
-      mqtt.on_message()
-      mqtt.on_connect()
-      #Run the code run()
-
 
 #Make a class to save the instance 
 class ESP:
@@ -32,62 +21,20 @@ class ESP:
     def set_pitch(self, pitch): 
         self.__pitch = pitch
 
-    
-#Call the values
-import ctypes
 #initialize the variable 
 Volume = 0
 #Get the memory address of volume
 MemoryAddressVolume=id(Volume)
 
-print(MemoryAddressVolume)
-#Get the value at at memory address that was 4
-ActualValue=ctypes.cast(MemoryAddressVolume,ctypes.py_object).value
-print(ActualValue)
+from class_sent import MQTT
 
-
-client = mqtt.Client()
-save = ESP()
-
-client.connect("mqtt.devbit.be",1883,60)
-client.subscribe("test/soundboard/esp1")
-client.subscribe("test/soundboard/esp2")
-#client.subscribe("test/soundboard/esp3")
-
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
- 
-  
-def on_message(client, userdata, msg):
-
-    
-    if(msg.topic == 'test/soundboard/esp1'):
-          save.set_volume = msg.payload
-          print(f"Message received [{msg.topic}]: {msg.payload}")
-          
-    
-    if(msg.topic == 'test/soundboard/esp2'):
-          save.set_pitch1 = msg.payload
-          print(f"Message received [{msg.topic}]: {msg.payload}")
-
-    if(msg.topic == 'test/soundboard/esp3'):
-          save.set_pitch2 = msg.payload
-          print(f"Message received [{msg.topic}]: {msg.payload}")
-
-  
-
-client.loop_start()
-
-# we call from client object , the on_connect method and we overwrited
-client.on_connect = on_connect
-client.on_message = on_message
+mqtt = MQTT("mqtt.devbit.be",1883, 60)
+topics = ["test/soundboard/esp1", "test/soundboard/esp2", "test/soundboard/esp3", "test/soundboard/esp4"]
+mqtt.subcribe(topics)
+mqtt.start()
 
 
 while(True):
       print('Buzzy...')
-      #Get the memory address
-      print(save.get_volume)
-      print(save.get_pitch)
-      
       time.sleep(1)
     
