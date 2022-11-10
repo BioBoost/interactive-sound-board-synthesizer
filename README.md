2199a6ec12444827e312e17e707b3b1cd9073f4d
Tristan De Lil <tristan.delil@student.vives.be>
2022-11-10 11:05:35 +0100

    changed-README

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # sound board interactive audio
 
-"""python
+"""bash
 python3 -m pip install synthesizer
 apt install portaudio19-dev
 pip install pyaudio
+"""
+
+"""text
+testing a thingy
 """
--- sound.py
+++ sound.py
@@ -1,14 +1,13 @@
 #import the MQTT class
 from mqttclient import MQTT
 from synthesize import Synthesize
-import time
 
 synth = Synthesize()
 
 mqtt = MQTT("mqtt.devbit.be",1883, 60)
 mqtt.subcribe("test/soundboard/esp1", "test/soundboard/esp2", "test/soundboard/esp3", "test/soundboard/esp4")
 mqtt.start()
-#e
+
 while(True):
 	print('Buzzy...')
 	#get values
