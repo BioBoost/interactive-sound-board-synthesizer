0b1fd615bb2decf8959a0a79f99bf339fe9be302
Nico De Witte <nico.dewitte@outlook.com>
2023-01-30 19:10:10 +0100

    Also allow bpm to be changed

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ sequencer = sq.Sequencer(4, notes)
 sequencer.set_bpm(360)
 sequencer.start()
 
-sonics = sa.SonicApi("127.0.0.1", "sonic")
+sonics = sa.SonicApi("192.168.1.99", "sonic")
 sonics.connect()
 
 deviceMapping = {
@@ -31,19 +31,22 @@ deviceMapping = {
   'esp32-sonic-e2c6c4': ('note', 1),
   'esp32-sonic-e29abc': ('note', 2),
   'esp32-sonic-e36270': ('note', 3),
-  # 'xxx': ('bpm')
+  'esp32-sonic-e37fa4': ('bpm', 0)
 }
 
-def on_device_update(id, distance):
-  print("Update: " + str(id) + " | " + str(distance))
+bpms = [ 30, 60, 120, 240, 360, 480]
 
+def on_device_update(id, distance):
   distance = min([distance, 100])
   if id in deviceMapping:
     config = deviceMapping[id]
     if config[0] == 'note':
       index = config[1]
-      noteIndex = int(len(notes) * distance / 100.0)
+      noteIndex = int((len(notes)-1) * distance / 100.0)
       sequencer.set_note(index, noteIndex)
+    elif config[0] == 'bpm':
+      bpmIndex = int((len(bpms)-1) * distance / 100.0)
+      sequencer.set_bpm(bpms[bpmIndex])
   else:
     print("No device mapping for " + id)
 
